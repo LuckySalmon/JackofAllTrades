c9b33288ca5c6ec519cfc02e8209960a15aacf55
Sam Edgar <LuckySalmon@users.noreply.github.com>
2019-05-14 14:31:51 -0600

    Create JackClasses.py

---
+++ JackClasses.py
@@ -0,0 +1,15 @@
+class 
+
+class Jacks():
+    def __init__(self, Name, Health, Defense, Speed):
+        self.Name = Name
+        self.Health = Health
+        self.Defense = Defense
+        self.Speed = Speed
+
+    def attack():
+        
+
+class PlumberJack(Jacks):
+    def __init__(self):
+        
