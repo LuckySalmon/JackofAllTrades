1db17d993c389cbfa864dc352056f01a7df31217
Sam Edgar <LuckySalmon@users.noreply.github.com>
2019-05-16 14:35:17 -0600

    This should be gotten rid of.

--- JackofAllTrades.py
+++ JackofAllTrades.py
@@ -5,52 +5,34 @@ import random
 
 
 class Moves:
-    def __init__(self, name, dmg, acc, status):
-        self.dmg = dmg
-        self.acc = acc
-        self.status = status
+    def __init__(self, Dmg, Acc, Status):
+        self.Dmg = Dmg
+        self.Acc = Acc
+        self.Status = Status
 
     def getDamage(self):
         return random.randint(self.dmg[0],self.dmg[1])
-    
     def getAccuracy(self):
-        return random.randint(self.acc[0],self.acc[1])
-    
+        return random.randint(self.Acc[0],self.Acc[1])
     def getStatus(self):
-        return status
+        return Status
 
     
 class Character:
-    
-    def __init__(self, Name, StartingHP, HP, Speed, Defence, XP):
+    def __init__(self, Name, StartingHP, HP, Speed, Defence, XP, level):
         self.StartingHP = StartingHP
         self.CurrentHP = HP
         self.Speed = Speed
         self.Defence = Defence
         self.XP = XP
         self.Name = Name
-        self.moveList =  []
-        self.Level = 1
-        
+        for i in range (XP/1000
+
     def moveList(self):
         #check what moves this person has and return a list of availible moves
-        print(self.moveList)
-
-    def addMove(self,name):
-        #check that the only the correct number of moves is added to the list and give options to replace a move
-        if self.moveList.len() <= int(0.41 * self.Level +4):
-            self.moveList.append(name)
-        else:
-            print("You have too many moves would you like to replace one?")
-            
-    
-    def getLevel(self):
-        T = self.level * 1000
-        if self.XP >= T:
-            self.Level +=1
-            self.XP = 0
-        return self.Level
-    
+        pass
+    def getLevel(XP):
+        
     def displayHP(self):
         print (self.CurrentHP)
 
