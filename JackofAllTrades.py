93ac2b73d25d61166cdfd2744dc6cb7d156c3484
BrassMonkay <50498533+BrassMonkay@users.noreply.github.com>
2019-05-16 14:18:57 -0600

    Update JackofAllTrades.py

--- JackofAllTrades.py
+++ JackofAllTrades.py
@@ -5,22 +5,23 @@ import random
 
 
 class Moves:
-    def __init__(self, Dmg, Acc, Status):
-        self.Dmg = Dmg
-        self.Acc = Acc
-        self.Status = Status
+    def __init__(self, name, dmg, acc, status):
+        self.dmg = dmg
+        self.acc = acc
+        self.status = status
 
     def getDamage(self):
         return random.randint(self.dmg[0],self.dmg[1])
+    
     def getAccuracy(self):
-        return random.randint(self.Acc[0],self.Acc[1])
+        return random.randint(self.acc[0],self.acc[1])
+    
     def getStatus(self):
-        return Status
+        return status
 
     
 class Character:
-    T = 1000
-    Level = 1
+    
     def __init__(self, Name, StartingHP, HP, Speed, Defence, XP):
         self.StartingHP = StartingHP
         self.CurrentHP = HP
@@ -28,14 +29,28 @@ class Character:
         self.Defence = Defence
         self.XP = XP
         self.Name = Name
-
+        self.moveList =  []
+        self.Level = 1
+        
     def moveList(self):
         #check what moves this person has and return a list of availible moves
-        pass
-    def getLevel(XP):
-        if XP >= T:
-            Level +=1
-        return Level 
+        print(self.moveList)
+
+    def addMove(self,name):
+        #check that the only the correct number of moves is added to the list and give options to replace a move
+        if self.moveList.len() <= int(0.41 * self.Level +4):
+            self.moveList.append(name)
+        else:
+            print("You have too many moves would you like to replace one?")
+            
+    
+    def getLevel(self):
+        T = self.level * 1000
+        if self.XP >= T:
+            self.Level +=1
+            self.XP = 0
+        return self.Level
+    
     def displayHP(self):
         print (self.CurrentHP)
 
