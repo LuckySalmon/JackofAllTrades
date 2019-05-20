dd2802cddb19d91d559234f3f5a33941c11eaec2
BrassMonkay <50498533+BrassMonkay@users.noreply.github.com>
2019-05-20 14:59:59 -0600

    Update JackofAllTrades.py

--- JackofAllTrades.py
+++ JackofAllTrades.py
@@ -21,7 +21,7 @@ class Move:
         return status
     
     def use(self):
-        success = self.getAccuracy() > random.randint(0, 99)
+        success = self.getAccuracy() > random.randint(0, 99) # we should make this a set vaulue and use that to get predictable odds or change based on a dodege value from opposing character
         damage = self.getDamage() if success else 0
         return success, damage
 
@@ -46,7 +46,7 @@ class Character:
         if len(self.moveList) <= int(0.41 * self.Level +4):
             self.moveList[move.name] = move
         else:
-<<<<<<< HEAD
+
             print("You have too many moves would you like to replace one?")
     
     def getLevel(self):
@@ -54,7 +54,7 @@ class Character:
         if self.XP >= T:
             self.Level +=1
             self.XP = 0
-=======
+
             print("You have too many moves. Would you like to replace one?")
             
     def updateLevel(self):
@@ -63,49 +63,77 @@ class Character:
             self.Level += 1
             self.XP -= threshold
             threshold = self.level * 1000
->>>>>>> 921a6e0ad27ad4d839a4cd1c89f81cf9a103aa05
+
         return self.Level
     
     def displayHP(self):
         print (self.Name + "'s HP:", self.HP)
 
 
-<<<<<<< HEAD
     #things we need
         #take dammage , use moves, die, various status affects, 
 
-=======
 def battle(ally, enemy):
     print(ally.Name, " VS ", enemy.Name)
+    
     for Character in (ally, enemy):
         Character.HP = Character.BaseHP
         Character.displayHP()
-    print('Select a move:')
-    for move in ally.moveList:
-        print(move)
-        dmg = ally.moveList[move].dmg
-        acc = ally.moveList[move].acc
-        print('\tDamage:', dmg[0], '-', dmg[1])
-        print('\tAccuracy:', acc[0], '-', acc[1])
-    selection = input('').title()
-    while not selection in ally.moveList:
-        selection = input('Please select a valid move.\n').title()
-    result = ally.moveList[selection].use()
-    if result[0]:
-        print(ally.Name + "'s", selection, 'hit for', result[1], 'damage!')
-    else:
-        print(ally.Name + "'s", selection, 'missed!')
-    enemy.HP -= result[1]
-    enemy.displayHP()
+
+    while ally.HP > 0 and enemy.HP > 0:
+        if ally.Speed > enemy.Speed:
+            print('Select a move %s:' %(ally.Name))
+            for move in ally.moveList:
+                print(move)
+                dmg = ally.moveList[move].dmg
+                acc = ally.moveList[move].acc
+                print('\tDamage:', dmg[0], '-', dmg[1])
+                print('\tAccuracy:', acc[0], '-', acc[1])
+            selection = input('').title()
+            
+            while not selection in ally.moveList:
+                selection = input('Please select a valid move.\n').title()
+            result = ally.moveList[selection].use()
+            
+            if result[0]:
+                print(ally.Name + "'s", selection, 'hit for', result[1], 'damage!')
+            else:
+                print(ally.Name + "'s", selection, 'missed!')
+            enemy.HP -= result[1]
+            enemy.displayHP()
+#2nd
+            print('Select a move %s:' %(enemy.Name))
+            for move in enemy.moveList:
+                print(move)
+                dmg = enemy.moveList[move].dmg
+                acc = enemy.moveList[move].acc
+                print('\tDamage:', dmg[0], '-', dmg[1])
+                print('\tAccuracy:', acc[0], '-', acc[1])
+            selection = input('').title()
+            
+            while not selection in ally.moveList:
+                selection = input('Please select a valid move.\n').title()
+            result = enemy.moveList[selection].use()
+            
+            if result[0]:
+                print(enemy.Name + "'s", selection, 'hit for', result[1], 'damage!')
+            else:
+                print(enemy.Name + "'s", selection, 'missed!')
+            ally.HP -= result[1]
+            ally.displayHP()
+
+        
         
 def test():
     flick = Move('flick', (0, 1), (100, 100))
     punch = Move('punch', (5, 15), (80, 95))
     spit = Move('spit', (0, 0), (40, 60), 'gross')
-    one = Character('one', 100, 1, 1, 1)
-    two = Character('two', 200, 2, 2, 2)
+    one = Character('one', 100, 2, 1, 1)
+    two = Character('two', 100, 1
+                    , 2, 2)
+    
     for character in (one, two):
         for move in (flick, punch, spit):
             character.addMove(move)
     battle(one, two)
->>>>>>> 921a6e0ad27ad4d839a4cd1c89f81cf9a103aa05
+
