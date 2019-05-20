bc673b4fe1d361690bc051b4efed535e9bd3fda8
Sam Edgar <LuckySalmon@users.noreply.github.com>
2019-05-20 00:15:03 -0600

    I just added some sound, since I thought it might be nice.

--- JackofAllTrades.py
+++ JackofAllTrades.py
@@ -1,7 +1,6 @@
-import random
+import random, winsound
 
-# The class "Moves" should be entirely self sufficient, and not require any numbers or
-#variables outside of the class.
+# The class "Moves" should be entirely self sufficient, and not require any numbers or variables outside of the class.
 
 
 class Moves:
@@ -11,12 +10,15 @@ class Moves:
         self.status = status
 
     def getDamage(self):
+        winsound.Beep(2000, 250)
         return random.randint(self.dmg[0],self.dmg[1])
     
     def getAccuracy(self):
+        winsound.Beep(2000, 250)
         return random.randint(self.acc[0],self.acc[1])
     
     def getStatus(self):
+        winsound.Beep(2000, 250)
         return status
 
     
@@ -35,13 +37,16 @@ class Character:
     def moveList(self):
         #check what moves this person has and return a list of availible moves
         print(self.moveList)
+        winsound.Beep(2000, 250)
 
     def addMove(self,name):
         #check that the only the correct number of moves is added to the list and give options to replace a move
         if self.moveList.len() <= int(0.41 * self.Level +4):
             self.moveList.append(name)
+            winsound.Beep(2000, 250)    # This makes a friendly little chirp whenever you successfully do it. The first argument is frequency (in hertz, I believe), and the second should be time in milliseconds.
         else:
-            print("You have too many moves would you like to replace one?")
+            print("You have too many moves. Would you like to replace one?")
+            winsound.Beep(800, 300)     # This also does a chirp, but different
             
     
     def getLevel(self):
@@ -49,8 +54,9 @@ class Character:
         if self.XP >= T:
             self.Level +=1
             self.XP = 0
+        winsound.Beep(2000, 250)
         return self.Level
     
     def displayHP(self):
         print (self.CurrentHP)
-
+        winsound.Beep(2000, 250)
