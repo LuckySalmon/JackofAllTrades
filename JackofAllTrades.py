7fd677deac0b31c242bacccb9fbab28a742869bb
WMOkiishi <w.muneo.o@gmail.com>
2019-05-22 19:17:26 -0600

    Update JackofAllTrades.py
    Some small changes and added basic functionality to defense

--- JackofAllTrades.py
+++ JackofAllTrades.py
@@ -2,24 +2,24 @@ import random, winsound, moves
     
 class Character:
     
-    def __init__(self, Name, BaseHP, Speed, Defense, XP):
+    def __init__(self, Name, BaseHP, Speed, Defense, XP=0):
         self.Name = Name.title()
         self.BaseHP = BaseHP
         self.HP = BaseHP
         self.Speed = Speed
         self.Defense = Defense
-        self.XP = XP
         self.moveList =  {}
+        self.XP = XP
         self.Level = 1
         
-    def moveList(self):
+    def moveList(self):    #isn't this redundant?
         '''check what moves this person has and return a list of availible moves'''
         print(self.moveList)
         return self.moveList
 
     def addMove(self, move):
         '''check that the only the correct number of moves is added to the list and give options to replace a move'''
-        if len(self.moveList) <= int(0.41 * self.Level +4):
+        if len(self.moveList) < int(0.41 * self.Level +4):     #changed this from <= as I'm assuming the formula is meant to be a cap, not one less than the cap
             self.moveList[move.name] = move
             winsound.Beep(600, 125)
             winsound.Beep(750, 100)
@@ -79,7 +79,9 @@ def battle(ally, enemy):
         
         i = (i+1) % 2
         opponent = characterList[i]
-        opponent.HP -= result[1]
+        damage = min(max(result[1] - opponent.Defense, 0), opponent.HP)   #is this how defense is supposed to work?
+        opponent.HP -= damage
+        print(opponent.Name, 'took', damage, 'damage!')
         opponent.displayHP()
         
     for character in characterList:
@@ -89,6 +91,15 @@ def battle(ally, enemy):
 def test():
     one = Character('one', 25, 1, 1, 1)
     two = Character('two', 25, 2, 2, 2)
+    
+    boxer = Character('boxer jack', 50, 2, 2)
+    boxer.addMove(moves.jab)
+    boxer.addMove(moves.cross)
+    boxer.addMove(moves.hook)
+    knife = Character('knife jack', 40, 2, 1)
+    knife.addMove(moves.shank)
+    knife.addMove(moves.stab)
+    knife.addMove(moves.slice)
     
     for character in (one, two):
         for move in (moves.flick, moves.punch, moves.spit):
